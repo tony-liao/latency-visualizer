0a0f07f0e63827a0197048f616d68f32f3ddb2da
Tony Liao <tony.liao.5966@gmail.com>
2016-06-20 14:54:06 +0000

    Add ioredis as dependency

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var Redis = require('ioredis');
+
 var app = express();
+var redis = new Redis(6379, '127.0.0.1');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000, function () {
