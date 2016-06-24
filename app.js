1376e17dc94acb6612a2551503dae4c932131ebb
Tony Liao <tony.liao.5966@gmail.com>
2016-06-24 11:47:20 +0000

    Add D3.js graph test

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var Redis = require('ioredis');
-var d3 = require('d3');
 
 var app = express();
 var redis = new Redis(6379, '127.0.0.1');
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/graph.js', function (req, res) {
+  res.sendFile(__dirname + '/graph.js')
+});
+
+app.get('/data.json', function (req, res) {
+  res.sendFile(__dirname + '/testData.json')
 });
 
 app.listen(3000, function () {
