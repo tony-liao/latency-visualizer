d29882f248341b728a3318cdcb2825d1d711b9a6
Tony Liao <tony.liao.5966@gmail.com>
2016-07-07 10:40:40 +0000

    Revert "change testcases"
    Need the old graph to notice certain issues (graph folding in on itself, not showing up, etc.)
    
    This reverts commit a89cffdcaa6559306d2431fe7b31fe5b768730a3.

--- test.js
+++ test.js
@@ -4,8 +4,11 @@ var redis = new Redis(6379, '127.0.0.1');
 module.exports = function() {
   console.log("setting up test");
   redis.flushdb();
-  redis.sadd('NodeSet', [ '2', '3', '1']);
-  redis.hmset('1', '2', 3.6, '3', 40.9);
-  redis.hmset('2', '1', 1.1, '3', 45.2);
-  redis.hmset('3', '1', 36.2, '2', 38.2);
+  redis.sadd('NodeSet', ['0', '1', '2', '3', '4', '5']);
+  redis.hmset('0', '1', 3.6, '2', 40.9, '3', 13.7, '4', 20.6, '5', 48.9);
+  redis.hmset('1', '0', 1.1, '2', 45.2);
+  redis.hmset('2', '0', 36.2, '1', 38.2, '5', 8.9);
+  redis.hmset('3', '0', 18.4, '4', 9.6);
+  redis.hmset('4', '0', 30.0, '3', 25.3);
+  redis.hmset('5', '0', 56.8, '2', 18);
 }
